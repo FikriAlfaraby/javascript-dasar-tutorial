12c56d84c45c28d67008e48bbd40b13e8c4f0839
Fikri Alfaraby <100327546+FikriAlfaraby@users.noreply.github.com>
2022-07-12 13:15:03 +0700

    change arrowFunction

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,9 +1,7 @@
-let mahasiswa = [1,2,3,4,5];
-let mahasiswa2 = [2,2,3,4,5];
+let mahasiswa = [1, 2, 3, 4, 5];
+let mahasiswa2 = [2, 2, 3, 4, 5];
 let reduce = (array) => {
- return array.reduce( (a,b) =>a+b);
-}
-let jumlahHuruf = reduce(mahasiswa)
-let jumlahHuruf2 = reduce(mahasiswa2)
-console.log(jumlahHuruf+jumlahHuruf2)
-
+  return array.reduce((a, b) => a + b);
+};
+let jumlahHuruf = reduce(mahasiswa);
+let jumlahHuruf2 = reduce(mahasiswa2);
